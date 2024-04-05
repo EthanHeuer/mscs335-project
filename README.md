e6aa4b12dc82edf4d5516e469e33270c76d36ca4
EthanHeuer <heuere9166@my.uwstout.edu>
2024-04-05 13:02:20 -0500

    Repo setup, midi samples, and read midi program

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# mscs335-project
-Final project for MSCS 335 - Machine Learning
+# MSCS 335 - Machine Learning Project
+
+## Description
+Final project for MSCS 335 - Machine Learning at UW-Stout.
+
+## Authors
+Ethan Heuer & Andrew Jevne
+
+## Installation
+
+## Usage
---
+++ programs/read-midi.py
@@ -0,0 +1,37 @@
+import mido
+import matplotlib.pyplot as plt
+
+class Note:
+    def __init__(self, note, velocity, start, end):
+        self.note = note
+        self.velocity = velocity
+        self.start = start
+        self.end = end
+
+
+SAMPLE = "Anno Domini - Hurting - 133 BPM F# Min Keys.mid"
+
+mid = mido.MidiFile(f"../samples/{SAMPLE}", clip=True)
+track = mid.tracks[0]
+
+notes = [[] for _ in range(128)]
+
+prev_time = 0
+for msg in track:
+    curr_time = msg.time + prev_time
+
+    if msg.type == "note_on":
+        notes[msg.note].append(Note(msg.note, msg.velocity, curr_time, curr_time))
+    elif msg.type == "note_off":
+        notes[msg.note][-1].end = curr_time
+
+    prev_time += msg.time
+
+for note in notes:
+    for n in note:
+        plt.plot([n.start, n.end], [n.note, n.note], color="black", linewidth=2)
+
+plt.title(SAMPLE)
+plt.xlabel("Time (ticks)")
+plt.ylabel("Midi Note")
+plt.show()
